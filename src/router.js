ded2a3b2bcf2a495a60bc9e15406805733d8772c
Andy Wang <wangyaodi@gmail.com>
2017-04-07 21:02:02 +0800

    add basic components

--- src/router.js
+++ src/router.js
@@ -2,15 +2,25 @@ import React from "react";
 import { Router, Route, IndexRoute } from "react-router";
 import { history } from "./store.js";
 import App from "./components/App";
-import Home from "./components/Home";
-import NotFound from "./components/NotFound";
+
+import Home from "./components/pages/HomePage";
+import NotFound from "./components/pages/NotFound";
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
+import RegisterPage from './components/pages/RegisterPage';
+import Dashboard from './components/pages/Dashboard';
 
 // build the router
 const router = (
-  <Router onUpdate={() => window.scrollTo(0, 0)} history={history}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
-      <Route path="*" component={NotFound}/>
+  <Router history={history}>
+    <Route component={App}>
+      <Route path="/" component={HomePage} />
+      <Route onEnter={checkAuth}>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Route>
+      <Route path="*" component={NotFound} />
     </Route>
   </Router>
 );
